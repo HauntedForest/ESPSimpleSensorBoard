641f15b2286933ffc9b7c39042f000509a8072f0
Eric Golde <eric@golde.org>
2021-01-30 15:27:24 -0800

    Added gulp command to compile and upload

--- Arduino/gulpfile.js
+++ Arduino/gulpfile.js
@@ -7,6 +7,18 @@ var cleancss = require('gulp-clean-css');
 var terser = require('gulp-terser');
 var gzip = require('gulp-gzip');
 var del = require('del');
+var exec = require('child_process').exec;
+//compile
+const COMPILE_OPTIONS = {
+	MKSPIFFS:
+		'C:\\Users\\eric\\AppData\\Local\\Arduino15\\packages\\esp8266\\tools\\mkspiffs\\2.5.0-4-b40a506\\mkspiffs.exe',
+	PYTHON3: 'C:\\Users\\eric\\AppData\\Local\\Arduino15\\packages\\esp8266\\tools\\python3\\3.7.2-post1\\python3.exe',
+	UPLOAD_PY:
+		'C:\\Users\\eric\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.6.3\\tools\\upload.py',
+
+	COM_PORT: 'COM5',
+	BAUD: '256000'
+};
 
 /* HTML Task */
 gulp.task('html', function() {
@@ -64,5 +76,38 @@ gulp.task('watch', function() {
 	gulp.watch('html/**/*.js', gulp.series('js'));
 });
 
+/*Compile SPIFFS*/
+gulp.task('spiffs-compile', function(cb) {
+	let cmd = COMPILE_OPTIONS.MKSPIFFS + ' -c data -p 256 -b 8192 -s 2076672 tmp/out.spiffs.bin';
+	console.log('Command: ' + cmd);
+	exec(cmd, function(err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		cb(err);
+	});
+});
+
+/* Spiffs - upload to device */
+gulp.task('spiffs-upload', function(cb) {
+	let cmd =
+		COMPILE_OPTIONS.PYTHON3 +
+		' ' +
+		COMPILE_OPTIONS.UPLOAD_PY +
+		' --chip esp8266 --port ' +
+		COMPILE_OPTIONS.COM_PORT +
+		' --baud ' +
+		COMPILE_OPTIONS.BAUD +
+		' write_flash 0x200000 tmp/out.spiffs.bin';
+	console.log('Command: ' + cmd);
+	exec(cmd, function(err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		cb(err);
+	});
+});
+
+/*Spiffs - Compile and upload to device */
+gulp.task('spiffs', gulp.series([ 'spiffs-compile', 'spiffs-upload' ]));
+
 /* Default Task */
-gulp.task('default', gulp.series([ 'clean', 'html', 'css', 'js', 'image' ]));
+gulp.task('default', gulp.series([ 'clean', 'html', 'css', 'js', 'image', 'spiffs' ]));
